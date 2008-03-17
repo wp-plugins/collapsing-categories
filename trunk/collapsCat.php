618c2b6707c158ee7973915db2f161bda6b63776
robfelty <robfelty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-03-17 14:40:26 +0000

    re-tagging version 0.3.7
    git-svn-id: https://plugins.svn.wordpress.org/collapsing-categories/tags/0.3.7@35202 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/collapsCat.php
@@ -0,0 +1,103 @@
+<?php
+/*
+Plugin Name: Collapsing Categories
+Plugin URI: http://blog.robfelty.com/plugins
+Description: Uses javascript to expand and collapse categories to show the posts that belong to the category 
+Author: Robert Felty
+Version: 0.3.7
+Author URI: http://robfelty.com
+
+Copyright 2007 Robert Felty
+
+This work is largely based on the Fancy Categories plugin by Andrew Rader
+(http://nymb.us), which was also distributed under the GPLv2. I have tried
+contacting him, but his website has been down for quite some time now. See the
+CHANGELOG file for more information.
+
+This file is part of Collapsing Categories
+
+		Collapsing Categories is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as published by 
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    Collapsing Categories is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Collapsing Categories; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/ 
+
+add_action( 'wp_head', array('collapsCat','get_head'));
+add_action('activate_collapsing-categories/collapsCat.php', array('collapsCat','init'));
+add_action('admin_menu', array('collapsCat','setup'));
+
+class collapsCat {
+
+	function init() {
+		if( function_exists('add_option') ) {
+			add_option( 'collapsCatShowPostCount', 'yes' );
+			add_option( 'collapsCatShowPages', 'no' );
+			add_option( 'collapsCatLinkToArchives', 'root' );
+			add_option( 'collapsCatSort', 'catName' );
+			add_option( 'collapsCatSortOrder', 'ASC' );
+		}
+	}
+
+	function setup() {
+		if( function_exists('add_options_page') ) {
+			add_options_page(__('Collapsing Categories'),__('Collapsing Categories'),1,basename(__FILE__),array('collapsCat','ui'));
+		}
+	}
+
+	function ui() {
+		include_once( 'collapsCatUI.php' );
+	}
+
+	function list_categories() {
+		global $wpdb;
+
+		include( 'collapsCatList.php' );
+
+		return;
+	}
+
+	function get_head() {
+		$url = get_settings('siteurl');
+    if (!function_exists('collapsArch')) {
+			echo "<script type=\"text/javascript\" src=\"$url/wp-content/plugins/collapsing-categories/collapsCat.js\"></script>\n";
+    echo "
+       <style type='text/css'>
+	/* a bit more style for the collapsing class used in the fancy categories and fancy archives */
+					 /*#sidebar ul ul li:before {content:'';}        */
+					 span.collapsing {border:0;
+						 padding:0; 
+						 margin:0; 
+						 cursor:pointer;
+						font-size:1.3em;
+					 }
+           #sidebar li.collapsing:before {content:'';} 
+          #sidebar li.collapsing {list-style-type:none}
+				 </style>
+					 ";
+    }
+    echo "
+       <style type='text/css'>
+          #sidebar li.collapsCatPost {padding:0 0 0 .1em;
+                         margin:0 0 0 1em;}
+				 </style>
+         ";
+		echo "<script type=\"text/javascript\">\n";
+		echo "// <![CDATA[\n";
+		echo "// These variables are part of the Collapsing Categories Plugin version: 0.3.7\n// Copyright 2007 Robert Felty (robfelty.com)\n";
+		echo "// ]]>\n</script>\n";
+	}
+}
+
+function collapsCat() {
+	collapsCat::list_categories();
+}
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,88 @@
+=== Collapsing Categories ===
+Contributors: robfelty
+Donate link: http://blog.robfelty.com/wordpress-plugins
+Tags: categories
+Requires at least: 2.2
+Tested up to: 2.3.3
+<<<<<<< .mine
+Stable tag: 0.3.7
+=======
+Stable tag: 0.3.7
+>>>>>>> .r35195
+
+This plugin uses Javascript to dynamically expand or collapsable the set of
+posts for each category.
+
+== Description ==
+
+This is a very simple plugin that uses Javascript to form a collapsable set of
+links in the sidebar for the categories. Every post corresponding to a given
+category will be expanded.
+
+It is largely based off of the Fancy Categories Plugin by Andrew Rader
+
+== Installation ==
+
+MANUAL INSTALLATION
+(the only option for pre 2.3 wordpress, unless you have the widget plugin installed)
+
+Unpackage contents to wp-content/plugins/ so that the files are in
+a collapsCat directory. Now enable the plugin. To use the plugin,
+change the following where appropriate	(most likely sidebar.php):
+
+	<ul>
+	 `<?php wp_list_cats(...); ?>`
+	</ul>
+
+To something of the following:
+`
+	<?php
+	  if( function_exists('collapsCat') ) {
+	  collapsCat();
+	} else {
+	  echo "<ul>\n";
+	  wp_list_cats(...);
+	  echo "</ul>\n";
+	}
+	?>
+`
+
+The above will fall back to the WP function for categories if you disable
+the plugin.
+
+WIDGET INSTALLATION
+
+For those who have widget capabilities, (default in Wordpress 2.3+), installation is easier. 
+
+Unpackage contents to wp-content/plugins/ so that the files are in a collapsCat
+directory. There should be 2 new plugins in your Wordpress Admin interface --
+Collapsing Categories, and Collapsing Categories Widget. You must enable both
+of them, in that order. Then simply go the Presentation > Widgets section and
+drag over the Collapsing Categories Widget.
+
+== Frequently Asked Questions ==
+
+None yet.
+
+== Screenshots ==
+
+1. available options 
+2. a few expanded categories with default theme
+
+== Demo ==
+
+I use this plugin in my blog at http://blog.robfelty.com
+
+
+== OPTIONS AND CONFIGURATIONS ==
+
+Options for Collapsing Categories are found under Options -> Collapsing
+Categories, the only option so far is the option to display post count.
+
+== CAVEAT ==
+
+Currently this plugin relies on Javascript to expand and collapse the links.
+If a user's browser doesn't support javascript they won't see the links to the
+posts, but the links to the categories will still work (which is the default
+behavior in wordpress anyways)
+
