a28dd89cd90c144a3b0f4a00ccf78646cebb7a18
robfelty <robfelty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-01 13:30:33 +0000

    put getSubCat function in its own class
    git-svn-id: https://plugins.svn.wordpress.org/collapsing-categories/trunk@37436 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- collapsCatList.php
+++ collapsCatList.php
@@ -74,76 +74,126 @@ if ($taxonomy==true) {
 $categories = $wpdb->get_results($catquery);
 $posts= $wpdb->get_results($postquery); 
 $parents=array();
-  foreach ($categories as $cat) {
-    if ($cat->parent!=0) {
-      array_push($parents, $cat->parent);
-    }
-  }
-  //echo "foobar here";
-  //echo "<pre>\n";
-  //print_r($parents);
-  //print_r($categories);
-  //print_r($posts);
-  
-  //echo "</pre>\n";
-  //$lastCat=-1;
-  foreach( $categories as $cat ) {
-    if ($cat->parent==0) {
-			$url = get_settings('siteurl');
-      $home=$url;
-      $lastCat= $cat->term_id;
-      // print out category name 
-			if ($taxonomy==true) {
-				//$link = "<a href='$url/category/".$cat->slug."' ";
-				$link = "<a href='".get_category_link($cat->term_id)."' ";
-			} else {
-				$link = "<a href='".get_category_link($cat->cat_ID)."' ";
-			}
-			if ( empty($cat->category_description) ) {
-			  $link .= 'title="'. sprintf(__("View all posts filed under %s"), wp_specialchars($cat->name)) . '"';
-			} else {
-				$link .= 'title="' . wp_specialchars(apply_filters('category_description',$cat->category_description,$cat)) . '"';
-			}
-			$link .= '>';
-			if ($taxonomy==true) {
-				$link .= apply_filters('list_cats', $cat->name, $cat).'</a>';
-			} else {
-				$link .= apply_filters('list_cats', $cat->cat_name, $cat).'</a>';
-			}
+foreach ($categories as $cat) {
+	if ($cat->parent!=0) {
+		array_push($parents, $cat->parent);
+	}
+}
+foreach( $categories as $cat ) {
+	if ($cat->parent==0) {
+		$url = get_settings('siteurl');
+		$home=$url;
+		$lastCat= $cat->term_id;
+		// print out category name 
+		if ($taxonomy==true) {
+			//$link = "<a href='$url/category/".$cat->slug."' ";
+			$link = "<a href='".get_category_link($cat->term_id)."' ";
+		} else {
+			$link = "<a href='".get_category_link($cat->cat_ID)."' ";
+		}
+		if ( empty($cat->category_description) ) {
+			$link .= 'title="'. sprintf(__("View all posts filed under %s"), wp_specialchars($cat->name)) . '"';
+		} else {
+			$link .= 'title="' . wp_specialchars(apply_filters('category_description',$cat->category_description,$cat)) . '"';
+		}
+		$link .= '>';
+		if ($taxonomy==true) {
+			$link .= apply_filters('list_cats', $cat->name, $cat).'</a>';
+		} else {
+			$link .= apply_filters('list_cats', $cat->cat_name, $cat).'</a>';
+		}
 
-      // TODO not sure why we are checking for this at all TODO
-			if( empty( $posts ) && empty($categories)) {
-				print( "<li class='collapsCat'><span class='collapsCat hide' onclick='expandCat(event); return false'>&#9660;&nbsp;</span>" );
+		// TODO not sure why we are checking for this at all TODO
+		if( empty( $posts ) && empty($categories)) {
+			print( "<li class='collapsCat'><span class='collapsCat hide' onclick='expandCat(event); return false'>&#9660;&nbsp;</span>" );
+		} else {
+			print( "<li class='collapsCat'><span class='collapsCat show' onclick='expandCat(event); return false'>&#9658;&nbsp;</span>" );
+		}
+		$subCatCount=0;
+		list ($subCatLinks, $subCatCount)=getSubCat($cat, $categories, $parents, $posts,$taxonomy,$subCatCount);
+		if( get_option('collapsCatShowPostCount')=='yes') {
+			if ($taxonomy==true) {
+				$link .= ' ('.intval($cat->count + $subCatCount).')';
 			} else {
-				print( "<li class='collapsCat'><span class='collapsCat show' onclick='expandCat(event); return false'>&#9658;&nbsp;</span>" );
+				$link .= ' ('.intval($cat->category_count).')';
 			}
-      $subCatCount=0;
-      list ($subCatLinks, $subCatCount)=getCollapsSubCat($cat, $categories, $parents, $posts,$taxonomy,$subCatCount);
-			if( get_option('collapsCatShowPostCount')=='yes') {
-				if ($taxonomy==true) {
-					$link .= ' ('.intval($cat->count + $subCatCount).')';
-				} else {
-					$link .= ' ('.intval($cat->category_count).')';
+		}
+		print( $link );
+		print( "\n<ul style=\"display:none;\">\n" );
+		echo $subCatLinks;
+		// Now print out the post info
+		if( ! empty($posts) ) {
+			foreach ($posts as $post) {
+				if ($post->term_id == $cat->term_id) {
+					$date=preg_replace("/-/", '/', $post->date);
+					$name=$post->post_name;
+					echo "<li class='collapsCatPost'><a href='$url/$archives$date/$name'>" .  strip_tags($post->post_title) . "</a></li>\n";
+					//echo "<li class='collapsCatPost'><a href='$url/$archives$date/$name'>" .  $post->post_title . "</a></li>\n";
 				}
-      }
-			print( $link );
-			print( "\n<ul style=\"display:none;\">\n" );
-      echo $subCatLinks;
-      // Now print out the post info
-      if( ! empty($posts) ) {
-        foreach ($posts as $post) {
-          if ($post->term_id == $cat->term_id) {
-            $date=preg_replace("/-/", '/', $post->date);
-            $name=$post->post_name;
-            echo "<li class='collapsCatPost'><a href='$url/$archives$date/$name'>" .  strip_tags($post->post_title) . "</a></li>\n";
-            //echo "<li class='collapsCatPost'><a href='$url/$archives$date/$name'>" .  $post->post_title . "</a></li>\n";
+			}
+			// close <ul> and <li> before starting a new category
+				echo "</ul>  </li> <!-- ending category -->\n";
+		} 
+	}
+}
+
+class getSubCat {
+function getSubCat($cat, $categories, $parents, $posts, $taxonomy,$subCatCount) {
+  if (in_array($cat->term_id, $parents)) {
+    foreach ($categories as $cat2) {
+      if ($cat->term_id==$cat2->parent) {
+        // print out category name 
+        $subCatLinks.=( "<li class='collapsCat'><span class='collapsCat show' onclick='expandCat(event); return false'>&#9658;&nbsp;</span>" );
+        if ($taxonomy==true) {
+          $link2 = "<a href='".get_category_link($cat2->term_id)."' ";
+          //$link2 = "<a href='$url/category/".$cat2->slug."' ";
+        } else {
+          $link2 = "<a href='".get_category_link($cat2->cat_ID)."' ";
+          //$link2 = "<a href=$url/'".get_category_link($cat2->cat_ID)."' ";
+        }
+        if ( empty($cat2->category_description) ) {
+          $link2 .= 'title="'. sprintf(__("View all posts filed under %s"), wp_specialchars($cat2->name)) . '"';
+        } else {
+          $link2 .= 'title="' . wp_specialchars(apply_filters('category_description',$cat2->category_description,$cat2)) . '"';
+        }
+        $link2 .= '>';
+        if ($taxonomy==true) {
+          $link2 .= apply_filters('list_cats', $cat2->name, $cat2).'</a>';
+        } else {
+          $link2 .= apply_filters('list_cats', $cat2->cat_name, $cat2).'</a>';
+        }
+
+        if( get_option('collapsCatShowPostCount')=='yes') {
+          if ($taxonomy==true) {
+            $link2 .= ' ('.intval($cat2->count).')';
+          } else {
+            $link2 .= ' ('.intval($cat2->category_count).')';
           }
         }
+        $subCatLinks.= $link2 ;
+        $subCatLinks.="\n<ul style=\"display:none;\">\n";
+        if (!in_array($cat2->term_id, $parents)) {
+          $subCatCount=$subCatCount+$cat2->count;
+        //if( !empty($posts)) {
+          foreach ($posts as $post2) {
+            if ($post2->term_id == $cat2->term_id) {
+              $date=preg_replace("/-/", '/', $post2->date);
+              $name=$post2->post_name;
+              $subCatLinks.= "<li class='collapsCatPost'><a href='$url/$archives$date/$name'>" .  strip_tags($post2->post_title) . "</a></li>\n";
+              //$subCatLinks.= "<li class='collapsCatPost'><a href='$url/$archives$date/$name'>" .  $post2->post_title . "</a></li>\n";
+            }
+          }
+        } else {
+          list ($subCatLink2, $subCatCount)= getSubCat($cat2, $categories, $parents, $posts,$taxonomy,$subCatCount);
+          $subCatLinks.="$subCatLink2";
+        }
         // close <ul> and <li> before starting a new category
-          echo "</ul>  </li> <!-- ending category -->\n";
-      } 
-		}
+        $subCatLinks.= "</ul>  </li> <!-- ending subcategory -->\n";
+      }
+    }
   }
-
+  return array($subCatLinks,$subCatCount);
+}
+}
 ?>
 </ul>
