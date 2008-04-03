c7404c2745062d79dd73ed9224b376ff6108d0a3
robfelty <robfelty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-03 14:21:49 +0000

    updating trunk to version 0.4.2
    git-svn-id: https://plugins.svn.wordpress.org/collapsing-categories/trunk@38026 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- collapsCatList.php
+++ collapsCatList.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Collapsing Categories version: 0.4.1
+Collapsing Categories version: 0.4.2
 Copyright 2007 Robert Felty
 
 This work is largely based on the Collapsing Categories plugin by Andrew Rader
@@ -86,114 +86,109 @@ function get_sub_cat($cat, $categories, $parents, $posts, $taxonomy,$subCatCount
 /* the category and tagging database structures changed drastically between wordpress 2.1 and 2.3. We will use different queries for category based vs. term_taxonomy based database structures */
 //$taxonomy=false;
 function list_categories() {
-		global $wpdb;
-if (get_option('collapsCatLinkToArchives')=='archives') {
-  $archives='archives.php/';
-} elseif (get_option('collapsCatLinkToArchives')=='index') {
-  $archives='index.php/';
-} elseif (get_option('collapsCatLinkToArchives')=='root') {
-  $archives='';
-}
-$taxonomy=true;
-$tables = $wpdb->query("show tables like 'wp_term_relationships'"); 
-if ($tables==0) {
-  $taxonomy=false;
-}
-$isPage='';
-if (get_option('collapsCatIncludePages'=='no')) {
-  $isPage="AND $wpdb->posts.post_type='post'";
-}
-if (get_option('collapsCatSort')!='') {
-  if (get_option('collapsCatSort')=='catName') {
-    $sortColumn="ORDER BY $wpdb->terms.name";
-  } elseif (get_option('collapsCatSort')=='catId') {
-    $sortColumn="ORDER BY $wpdb->terms.term_id";
+  global $wpdb;
+  if (get_option('collapsCatLinkToArchives')=='archives') {
+    $archives='archives.php/';
+  } elseif (get_option('collapsCatLinkToArchives')=='index') {
+    $archives='index.php/';
+  } elseif (get_option('collapsCatLinkToArchives')=='root') {
+    $archives='';
   }
-  $sortOrder = get_option('collapsCatSortOrder');
-} 
+  $taxonomy=true;
+  $tables = $wpdb->query("show tables like 'wp_term_relationships'"); 
+  if ($tables==0) {
+    $taxonomy=false;
+  }
+  $isPage='';
+  if (get_option('collapsCatIncludePages'=='no')) {
+    $isPage="AND $wpdb->posts.post_type='post'";
+  }
+  if (get_option('collapsCatSort')!='') {
+    if (get_option('collapsCatSort')=='catName') {
+      $sortColumn="ORDER BY $wpdb->terms.name";
+    } elseif (get_option('collapsCatSort')=='catId') {
+      $sortColumn="ORDER BY $wpdb->terms.term_id";
+    }
+    $sortOrder = get_option('collapsCatSortOrder');
+  } 
 
+  echo "<ul id='collapsCatList'>\n";
 
+  if ($taxonomy==true) {
+      $catquery = "SELECT $wpdb->terms.term_id, $wpdb->terms.name, $wpdb->terms.slug, $wpdb->term_taxonomy.count, $wpdb->term_taxonomy.parent FROM $wpdb->terms, $wpdb->term_taxonomy WHERE $wpdb->terms.term_id = $wpdb->term_taxonomy.term_id AND $wpdb->term_taxonomy.count >0 AND $wpdb->terms.name != 'Blogroll' AND $wpdb->term_taxonomy.taxonomy = 'category' $sortColumn $sortOrder";
+      $postquery = "SELECT $wpdb->terms.term_id, $wpdb->terms.name, $wpdb->terms.slug, $wpdb->term_taxonomy.count, $wpdb->posts.id, $wpdb->posts.post_title, $wpdb->posts.post_name, date($wpdb->posts.post_date) as 'date' FROM $wpdb->posts, $wpdb->terms, $wpdb->term_taxonomy, $wpdb->term_relationships  WHERE $wpdb->posts.id = $wpdb->term_relationships.object_id AND $wpdb->posts.post_status='publish' AND $wpdb->terms.term_id = $wpdb->term_taxonomy.term_id AND $wpdb->term_relationships.term_taxonomy_id = $wpdb->term_taxonomy.term_taxonomy_id $isPage";
+  } else {
+    $catquery = "SELECT cat_ID, cat_name, category_nicename, category_description, category_parent, category_count FROM $wpdb->categories WHERE cat_ID > 0 AND category_parent = 0 AND category_count > 0";
+    $postquery = "SELECT $wpdb->posts.post_title, $wpdb->posts.post_name, DATE($wpdb->posts.post_date) AS 'date' FROM $wpdb->posts, $wpdb->post2cat, $wpdb->categories where $wpdb->post2cat.category_id = $cat->cat_ID and $wpdb->posts.ID = $wpdb->post2cat.post_id and $wpdb->categories.cat_ID = $wpdb->post2cat.category_id and $wpdb->posts.post_status = 'publish' and $wpdb->categories.category_count>0 $isPage";
+  }
+    /* changing to use only one query 
+     * don't forget to exclude pages if so desired
+     */
+  $categories = $wpdb->get_results($catquery);
+  $posts= $wpdb->get_results($postquery); 
+  $parents=array();
+  foreach ($categories as $cat) {
+    if ($cat->parent!=0) {
+      array_push($parents, $cat->parent);
+    }
+  }
+  foreach( $categories as $cat ) {
+    if ($cat->parent==0) {
+      $url = get_settings('siteurl');
+      $home=$url;
+      $lastCat= $cat->term_id;
+      // print out category name 
+      if ($taxonomy==true) {
+        //$link = "<a href='$url/category/".$cat->slug."' ";
+        $link = "<a href='".get_category_link($cat->term_id)."' ";
+      } else {
+        $link = "<a href='".get_category_link($cat->cat_ID)."' ";
+      }
+      if ( empty($cat->category_description) ) {
+        $link .= 'title="'. sprintf(__("View all posts filed under %s"), wp_specialchars($cat->name)) . '"';
+      } else {
+        $link .= 'title="' . wp_specialchars(apply_filters('category_description',$cat->category_description,$cat)) . '"';
+      }
+      $link .= '>';
+      if ($taxonomy==true) {
+        $link .= apply_filters('list_cats', $cat->name, $cat).'</a>';
+      } else {
+        $link .= apply_filters('list_cats', $cat->cat_name, $cat).'</a>';
+      }
+
+      // TODO not sure why we are checking for this at all TODO
+      if( empty( $posts ) && empty($categories)) {
+        print( "<li class='collapsCat'><span class='collapsCat hide' onclick='expandCat(event); return false'>&#9660;&nbsp;</span>" );
+      } else {
+        print( "<li class='collapsCat'><span class='collapsCat show' onclick='expandCat(event); return false'>&#9658;&nbsp;</span>" );
+      }
+      $subCatCount=0;
+      list ($subCatLinks, $subCatCount)=get_sub_cat($cat, $categories, $parents, $posts,$taxonomy,$subCatCount);
+      if( get_option('collapsCatShowPostCount')=='yes') {
+        if ($taxonomy==true) {
+          $link .= ' ('.intval($cat->count + $subCatCount).')';
+        } else {
+          $link .= ' ('.intval($cat->category_count).')';
+        }
+      }
+      print( $link );
+      print( "\n<ul style=\"display:none;\">\n" );
+      echo $subCatLinks;
+      // Now print out the post info
+      if( ! empty($posts) ) {
+        foreach ($posts as $post) {
+          if ($post->term_id == $cat->term_id) {
+            $date=preg_replace("/-/", '/', $post->date);
+            $name=$post->post_name;
+            echo "<li class='collapsCatPost'><a href='$url/$archives$date/$name'>" .  strip_tags($post->post_title) . "</a></li>\n";
+            //echo "<li class='collapsCatPost'><a href='$url/$archives$date/$name'>" .  $post->post_title . "</a></li>\n";
+          }
+        }
+        // close <ul> and <li> before starting a new category
+          echo "</ul>  </li> <!-- ending category -->\n";
+      } 
+    }
+  }
+  echo "</ul>\n";
+}
 ?>
-
-<ul id="collapsCatList">
-<?php
-    global $wpdb;
-
-if ($taxonomy==true) {
-    $catquery = "SELECT $wpdb->terms.term_id, $wpdb->terms.name, $wpdb->terms.slug, $wpdb->term_taxonomy.count, $wpdb->term_taxonomy.parent FROM $wpdb->terms, $wpdb->term_taxonomy WHERE $wpdb->terms.term_id = $wpdb->term_taxonomy.term_id AND $wpdb->term_taxonomy.count >0 AND $wpdb->terms.name != 'Blogroll' AND $wpdb->term_taxonomy.taxonomy = 'category' $sortColumn $sortOrder";
-    $postquery = "SELECT $wpdb->terms.term_id, $wpdb->terms.name, $wpdb->terms.slug, $wpdb->term_taxonomy.count, $wpdb->posts.id, $wpdb->posts.post_title, $wpdb->posts.post_name, date($wpdb->posts.post_date) as 'date' FROM $wpdb->posts, $wpdb->terms, $wpdb->term_taxonomy, $wpdb->term_relationships  WHERE $wpdb->posts.id = $wpdb->term_relationships.object_id AND $wpdb->posts.post_status='publish' AND $wpdb->terms.term_id = $wpdb->term_taxonomy.term_id AND $wpdb->term_relationships.term_taxonomy_id = $wpdb->term_taxonomy.term_taxonomy_id $isPage";
-} else {
-  $catquery = "SELECT cat_ID, cat_name, category_nicename, category_description, category_parent, category_count FROM $wpdb->categories WHERE cat_ID > 0 AND category_parent = 0 AND category_count > 0";
-  $postquery = "SELECT $wpdb->posts.post_title, $wpdb->posts.post_name, DATE($wpdb->posts.post_date) AS 'date' FROM $wpdb->posts, $wpdb->post2cat, $wpdb->categories where $wpdb->post2cat.category_id = $cat->cat_ID and $wpdb->posts.ID = $wpdb->post2cat.post_id and $wpdb->categories.cat_ID = $wpdb->post2cat.category_id and $wpdb->posts.post_status = 'publish' and $wpdb->categories.category_count>0 $isPage";
-}
-  /* changing to use only one query 
-   * don't forget to exclude pages if so desired
-   */
-$categories = $wpdb->get_results($catquery);
-$posts= $wpdb->get_results($postquery); 
-$parents=array();
-foreach ($categories as $cat) {
-	if ($cat->parent!=0) {
-		array_push($parents, $cat->parent);
-	}
-}
-foreach( $categories as $cat ) {
-	if ($cat->parent==0) {
-		$url = get_settings('siteurl');
-		$home=$url;
-		$lastCat= $cat->term_id;
-		// print out category name 
-		if ($taxonomy==true) {
-			//$link = "<a href='$url/category/".$cat->slug."' ";
-			$link = "<a href='".get_category_link($cat->term_id)."' ";
-		} else {
-			$link = "<a href='".get_category_link($cat->cat_ID)."' ";
-		}
-		if ( empty($cat->category_description) ) {
-			$link .= 'title="'. sprintf(__("View all posts filed under %s"), wp_specialchars($cat->name)) . '"';
-		} else {
-			$link .= 'title="' . wp_specialchars(apply_filters('category_description',$cat->category_description,$cat)) . '"';
-		}
-		$link .= '>';
-		if ($taxonomy==true) {
-			$link .= apply_filters('list_cats', $cat->name, $cat).'</a>';
-		} else {
-			$link .= apply_filters('list_cats', $cat->cat_name, $cat).'</a>';
-		}
-
-		// TODO not sure why we are checking for this at all TODO
-		if( empty( $posts ) && empty($categories)) {
-			print( "<li class='collapsCat'><span class='collapsCat hide' onclick='expandCat(event); return false'>&#9660;&nbsp;</span>" );
-		} else {
-			print( "<li class='collapsCat'><span class='collapsCat show' onclick='expandCat(event); return false'>&#9658;&nbsp;</span>" );
-		}
-		$subCatCount=0;
-		list ($subCatLinks, $subCatCount)=get_sub_cat($cat, $categories, $parents, $posts,$taxonomy,$subCatCount);
-		if( get_option('collapsCatShowPostCount')=='yes') {
-			if ($taxonomy==true) {
-				$link .= ' ('.intval($cat->count + $subCatCount).')';
-			} else {
-				$link .= ' ('.intval($cat->category_count).')';
-			}
-		}
-		print( $link );
-		print( "\n<ul style=\"display:none;\">\n" );
-		echo $subCatLinks;
-		// Now print out the post info
-		if( ! empty($posts) ) {
-			foreach ($posts as $post) {
-				if ($post->term_id == $cat->term_id) {
-					$date=preg_replace("/-/", '/', $post->date);
-					$name=$post->post_name;
-					echo "<li class='collapsCatPost'><a href='$url/$archives$date/$name'>" .  strip_tags($post->post_title) . "</a></li>\n";
-					//echo "<li class='collapsCatPost'><a href='$url/$archives$date/$name'>" .  $post->post_title . "</a></li>\n";
-				}
-			}
-			// close <ul> and <li> before starting a new category
-				echo "</ul>  </li> <!-- ending category -->\n";
-		} 
-	}
-}
-}
-?>
-</ul>
