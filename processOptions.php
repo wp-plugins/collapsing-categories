20d41c2e605f80454e9b2421dfdd455c80070904
robfelty <robfelty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-25 23:36:12 +0000

    adding process options
    git-svn-id: https://plugins.svn.wordpress.org/collapsing-categories/tags/0.7@75904 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ processOptions.php
@@ -0,0 +1,38 @@
+<?php
+ if ( -1 == $number ) {
+    /* default options go here */
+    $title = 'Categories';
+    $text = '';
+    $showPostCount = 'yes';
+    $catSort = 'catName';
+    $catSortOrder = 'ASC';
+    $postSort = 'postTitle';
+    $postSortOrder = 'ASC';
+    $defaultExpand='';
+    $number = '%i%';
+    $expand='1';
+    $inExclude='include';
+    $inExcludeCats='';
+    $showPosts='yes';
+    $linkToCat='yes';
+    $showPages='no';
+    $animate='1';
+    $catfeed='none';
+  } else {
+    $title = attribute_escape($options[$number]['title']);
+    $showPostCount = $options[$number]['showPostCount'];
+    $expand = $options[$number]['expand'];
+    $inExcludeCats = $options[$number]['inExcludeCats'];
+    $inExclude = $options[$number]['inExclude'];
+    $catSort = $options[$number]['catSort'];
+    $catSortOrder = $options[$number]['catSortOrder'];
+    $postSort = $options[$number]['postSort'];
+    $postSortOrder = $options[$number]['postSortOrder'];
+    $defaultExpand = $options[$number]['defaultExpand'];
+    $showPosts = $options[$number]['showPosts'];
+    $showPages = $options[$number]['showPages'];
+    $linkToCat = $options[$number]['linkToCat'];
+    $animate = $options[$number]['animate'];
+    $catfeed = $options[$number]['catfeed'];
+  }
+?>
