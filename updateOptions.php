03acbee50c5419a810f5bde12c4bc41b4f159557
robfelty <robfelty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-26 02:36:02 +0000

    putting in trunk
    git-svn-id: https://plugins.svn.wordpress.org/collapsing-categories/trunk@75922 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ updateOptions.php
@@ -0,0 +1,76 @@
+<?php
+foreach ( (array) $_POST['collapsCat'] as $widget_number => $widget_collapsCat ) {
+  if ( !isset($widget_collapsCat['title']) && isset($options[$widget_number]) ) // user clicked cancel
+    continue;
+  $title = strip_tags(stripslashes($widget_collapsCat['title']));
+  $catSortOrder= 'ASC' ;
+  if($widget_collapsCat['catSortOrder'] == 'DESC') {
+    $catSortOrder= 'DESC' ;
+  }
+  $showPosts= 'yes' ;
+  if($widget_collapsCat['showPosts'] == 'no') {
+    $showPosts= 'no' ;
+  }
+  $linkToCat= 'yes' ;
+  if($widget_collapsCat['linkToCat'] == 'no') {
+    $linkToCat= 'no' ;
+  }
+  $showPostCount= 'no' ;
+  if( isset($widget_collapsCat['showPostCount'])) {
+    $showPostCount= 'yes' ;
+  }
+  $showPages= 'no' ;
+  if( isset($widget_collapsCat['showPages'])) {
+    $showPages= 'yes' ;
+  }
+  if($widget_collapsCat['catSort'] == 'catName') {
+    $catSort= 'catName' ;
+  } elseif ($widget_collapsCat['catSort'] == 'catId') {
+    $catSort= 'catId' ;
+  } elseif ($widget_collapsCat['catSort'] == 'catSlug') {
+    $catSort= 'catSlug' ;
+  } elseif ($widget_collapsCat['catSort'] == 'catOrder') {
+    $catSort= 'catOrder' ;
+  } elseif ($widget_collapsCat['catSort'] == 'catCount') {
+    $catSort= 'catCount' ;
+  } elseif ($widget_collapsCat['catSort'] == '') {
+    $catSort= '' ;
+    $catSortOrder= '' ;
+  }
+  $postSortOrder= 'ASC' ;
+  if($widget_collapsCat['postSortOrder'] == 'DESC') {
+    $postSortOrder= 'DESC' ;
+  }
+  if($widget_collapsCat['postSort'] == 'postTitle') {
+    $postSort= 'postTitle' ;
+  } elseif ($widget_collapsCat['postSort'] == 'postId') {
+    $postSort= 'postId' ;
+  } elseif ($widget_collapsCat['postSort'] == 'postComment') {
+    $postSort= 'postComment' ;
+  } elseif ($widget_collapsCat['postSort'] == 'postDate') {
+    $postSort= 'postDate' ;
+  } elseif ($widget_collapsCat['postSort'] == '') {
+    $postSort= '' ;
+    $postSortOrder= '' ;
+  }
+  $expand= $widget_collapsCat['expand'];
+  $catfeed= $widget_collapsCat['catfeed'];
+  $inExclude= 'include' ;
+  if($widget_collapsCat['inExclude'] == 'exclude') {
+    $inExclude= 'exclude' ;
+  }
+  $animate='1';
+  if( !isset($widget_collapsCat['animate'])) {
+    $animate= '0' ;
+  }
+  $inExcludeCats=addslashes($widget_collapsCat['inExcludeCats']);
+  $defaultExpand=addslashes($widget_collapsCat['defaultExpand']);
+  $options[$widget_number] = compact( 'title','showPostCount','catSort',
+      'catSortOrder','defaultExpand','expand','inExclude', 'showPosts',
+      'inExcludeCats','postSort','postSortOrder','showPages', 'linkToCat',
+      'catfeed','animate' );
+}
+
+update_option('collapsCatOptions', $options);
+$updated = true;
+?>
