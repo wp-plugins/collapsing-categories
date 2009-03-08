902b83b6fae4f0ec3bf000d68ae8a948afb495a0
robfelty <robfelty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-08 19:54:48 +0000

    version 0.9.3
    git-svn-id: https://plugins.svn.wordpress.org/collapsing-categories/trunk@100486 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- collapscat.js
+++ collapscat.js
@@ -1,5 +1,5 @@
 /*
-Collapsing Categories version: 0.9.2
+Collapsing Categories version: 0.9.3
 Copyright 2007 Robert Felty
 
 This work is largely based on the Fancy Categories plugin by Andrew Rader
