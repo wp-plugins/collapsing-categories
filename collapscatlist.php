bbc1a0a58aa20b9e2d1993167c2a4b7c22993467
robfelty <robfelty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-22 20:58:51 +0000

    got rid of custom urls
    git-svn-id: https://plugins.svn.wordpress.org/collapsing-categories/trunk@138068 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- collapscatlist.php
+++ collapscatlist.php
@@ -129,7 +129,7 @@ function getSubPosts($posts, $cat2, $subCatPosts, $showPosts) {
           }
           $posttext2.= "<li class='collapsCatPost'><a $self
               href='".get_permalink($post2).
-              "?nav=collapsing-categories' title='$title_text'>$linktext</a></li>\n";
+              " title='$title_text'>$linktext</a></li>\n";
         }
       }
     }
@@ -578,7 +578,7 @@ $wpdb->term_taxonomy AS tt ON t.term_id = tt.term_id WHERE tt.taxonomy IN
                   }
 									$posttext.= "<li class='collapsCatPost'><a $self
 										href='".get_permalink($post).
-										"?nav=collapsing-categories' title='$title_text'>$linktext</a></li>\n";
+										" title='$title_text'>$linktext</a></li>\n";
 								} 
               }
             // close <ul> and <li> before starting a new category
